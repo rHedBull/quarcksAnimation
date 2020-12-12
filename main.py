d47fc7b9402db7b1930e40235f0352aad48e86b9
Hendrik <hendrik.wirthwein@gmail.com>
2020-12-13 00:07:34 +0100

    improved weakInteraction animation

--- main.py
+++ main.py
@@ -12,7 +12,26 @@ def main():
 
 def weakInteraction(wind):
     initialX = 0.1 * wind.getWidth() ## start point UpQuarck
-    initialY = 0.9 * wind.getHeight()
+    initialY = 0.8 * wind.getHeight()
+
+    ## time arrow ##
+    pointUpArrow = Point(40, (wind.getHeight()*0.9))
+    pointDownArrow = Point(40, (wind.getHeight()*0.1))
+    timeArrow = Line(pointUpArrow, pointDownArrow)
+    timeArrow.setArrow("last")
+    timeArrow.setFill("white")
+    timeArrow.setWidth(5)
+    timeArrow.draw(wind)
+
+    pHalfArrow = Point(50, (wind.getHeight()*0.5))
+    timeText = Text(pHalfArrow, "t")
+    timeText.setFill("white")
+    timeText.setSize(30)
+    timeText.draw(wind)
+
+    ## überschrift
+    ## relative to window size !! 
+
 
     moveUpQX = 100
     moveUpQY = -100
@@ -28,17 +47,34 @@ def weakInteraction(wind):
     p2 = Point(initialWX, initialWY)
     w = WplusBoson(p2)
     w.draw(wind)
+    ## u.toAnti() create current x and y position attributes for all classes, change draw, undraw, to Anti methods !!!!!!
 
     moveWX = 100
     moveWY = -100  
     w.move(moveWX, moveWY)
+    
+
+    initialNX = initialWX + moveWX
+    initialNY = initialWY + moveWY
+    p3 = Point(initialNX, initialNY)
+    n = ElektronNeutrino(p3)
+    n.draw(wind)
+
+    positron = Positron(p3)
+    positron.draw(wind)
+    w.unDraw()
+
+    moveNX = 100
+    moveNY = -100  
+    n.move(moveNX, moveNY)
+    positron.move(-moveNX, moveNY)
+
 
     u.move(-moveUpQX, moveUpQY)
 
-    
+
 
 
 
 
 main()
-window.getMouse()
